c2d443461f17fcc28f1f6bc35ac0e35179bc3fcb
chaz102 <138363749+chaz102@users.noreply.github.com>
2025-06-23 02:00:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
+from dotenv import load_dotenv
 import os, requests
 
 app = Flask(__name__)
 
+# Load pipelines
 sentiment = pipeline("text-classification", model="Chaz1003/FELBERT")
 summarizer = pipeline("summarization", model="google/pegasus-xsum")
-LTR_URL = "http://localhost:5000"
-# LTR_URL = "https://libretranslate.de"
+
+LTR_URL = os.getenv("LTR_URL", "http://localhost:5000")
 
 @app.route("/batch-analyze", methods=["POST"])
 def batch_analyze():
@@ -60,4 +62,5 @@ def batch_analyze():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
