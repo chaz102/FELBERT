c6ce7eef79d441c38f178e2c651dc23017d1c44c
chaz102 <138363749+chaz102@users.noreply.github.com>
2025-06-25 04:44:03 +0800

    Add files via upload
    updated cors

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from transformers import pipeline
 import os
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Load sentiment model
 sentiment = pipeline("text-classification", model="Chaz1003/FELBERT")
